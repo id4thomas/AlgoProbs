8877164762f19068ac200f5351c8261750da01af
Youngrok Song <id2thomas@gmail.com>
2022-03-24 20:49:37 +0900

    leetcode 70 - update better solution

--- Leetcode/70.py
+++ Leetcode/70.py
@@ -9,7 +9,8 @@ from math import comb
 
 class Solution:
     # def climbStairs(self, n: int) -> int:
-    def climbStairs(self, n):
+    # Don't use combination equations
+    def climbStairs_bad(self, n):
         # 1 or 2
         # 3: 1,2 / 2,1 / 1,1,1
         # 5: 2,2,1 / 2,1,2 / 1,2,2 / 2,2,1,1,1 /
@@ -24,10 +25,27 @@ class Solution:
         
         for i in range(k,0,-1):
             climb_count += comb(count_pos,i)
-        
-        print(k)
 
         return
+    
+    def climbStairs(self, n):
+        if n<3:
+            return n
+        
+        # Only use O(1) memory
+        arr = [0]*3
+        
+        arr[0] = 1
+        arr[1] = 2
+        
+        for i in range(2,n):
+            arr[2] = arr[0] + arr[1]
+            
+            # Overwrite
+            arr[0] = arr[1]
+            arr[1] = arr[2]
+            
+        return arr[2]
 # 2
 n = 2
 Solution().climbStairs(n)
